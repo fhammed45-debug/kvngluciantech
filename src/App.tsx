cc3ff240731b9ea1c2e9daf867d66e8eb79b36b0
Fatai Hammed <fhammed45-debug>
2025-12-05 20:59:45 +0100

    Add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,38 @@ import ForgotPassword from './ForgotPassword';
 import VerifyEmail from './VerifyEmail';
 import About from './About';
 import Service from './Service';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/Home" replace />} />
       <Route path="/Home" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
+      
+      {/* Public routes - no login required */}
       <Route path="/Login" element={<Login />} />
-      <Route path="/ForgotPassword" element={<ForgotPassword />} />  {/* Added / */}
       <Route path="/Signup" element={<Signup />} />
+      <Route path="/ForgotPassword" element={<ForgotPassword />} />
       <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/About" element={<About />} />
-       <Route path="/Service" element={<Service />} />
+      
+      {/* Protected routes - login required */}
+      <Route path="/contact" element={
+        <ProtectedRoute>
+          <Contact />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/About" element={
+        <ProtectedRoute>
+          <About />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/Service" element={
+        <ProtectedRoute>
+          <Service />
+        </ProtectedRoute>
+      } />
     </Routes>
   );
 }
